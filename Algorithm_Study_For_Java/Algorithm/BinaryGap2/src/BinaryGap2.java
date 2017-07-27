4791621ef4b6b9f0fee2531467fedb5d6c3a65ed
SeonghoHaam <haams703@naver.com>
2017-07-28 01:19:03 +0900

    Update BinaryGap2.java

--- Algorithm_Study_For_Java/Algorithm/BinaryGap2/src/BinaryGap2.java
+++ Algorithm_Study_For_Java/Algorithm/BinaryGap2/src/BinaryGap2.java
@@ -1,45 +1,41 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public class BinaryGap2 {
+public class BinaryGap2 { 
+	// codility에서 class명 Solution으로 바꿔서 돌려주세요! 그래야 돌아갑니다..ㅠ
+
+	static int solution(int N) {
+		ArrayList<Integer> arrList = new ArrayList<>();
+		int cnt = 0;
+		int idx = 0;
+		int max = 0;
+		int i=N;
+		
+		do{
+			arrList.add(idx,i%2);
+			i = (int)i/2;
+			idx++;
+		}while(i>0);
+	
+		
+		for (int j = arrList.size()-1 ; j >= 0 ; j--) {
+
+			if (arrList.get(j) == 0) {
+				cnt++;
+			} else {
+				if (max < cnt) {
+					max = cnt;
+				}
+				cnt = 0;
+			}
+		}
+		//System.out.print(max);
+		return max;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		
-		System.out.println(solution(n));
+		//solution(sc.nextInt());
+		System.out.println(solution(sc.nextInt()));
 	}
-	
-	public static int solution(int N)
-	{
-		int num = N;
-		int stack_size;
-		int big = 0, total = 0;
-		Stack<Integer> stack_binary = new Stack<Integer>();
-		
-		while(num > 1)
-		{
-			stack_binary.push(num%2);
-			num = num / 2;
-		}
-		stack_binary.push(num);
-		
-		stack_size = stack_binary.size();
-		
-		for(int i = stack_size - 1; i>=0 ;i--)
-		{
-			if(stack_binary.elementAt(i) == 1)
-			{
-				if(total >= big)
-					big = total;
-				total = 0;
-			}
-			else
-				total += 1;
-		}
-		
-		return big;
-		
-	}
-
 }
